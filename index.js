4e7d6e4c244abf9758dd4cc922af0507434f37ae
icarodredd <icarog854@gmail.com>
2024-11-13 16:02:00 -0300

    missing redirect

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const button = document.getElementById("submit");
 const inputUrl = document.querySelector("input");
+const section = document.querySelector("section");
 
 button.addEventListener("click", async (e) => {
   e.preventDefault();
 
-  await fetch(`https://unshorten.but.rest/${inputUrl.value}`).then(
-    async (res) => console.log(await res.json())
-  );
+  const originUrl = await fetch(
+    `https://unshorten.but.rest/${inputUrl.value}`
+  ).then(async (res) => await res.json());
+  /*     .then((json) => json.resolved_url); */
+  const resultText = document.createElement("p");
+  const resultUrl = document.createElement("a");
+
+  resultUrl.setAttribute("href", originUrl);
+  resultUrl.setAttribute("target", "_blank");
+  resultUrl.textContent += `${originUrl.resolved_url}`;
+
+  resultText.textContent += "Original link:";
+  section.appendChild(resultText);
+  section.appendChild(resultUrl);
 });
