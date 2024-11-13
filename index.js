3c2877a14e252fccf0b85864f56d5e6bd043e6b3
icarodredd <icarog854@gmail.com>
2024-11-13 15:00:56 -0300

    unshort url sucess

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello!");
+const button = document.getElementById("submit");
+const inputUrl = document.querySelector("input");
+
+button.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  await fetch(`https://unshorten.but.rest/${inputUrl.value}`).then(
+    async (res) => console.log(await res.json())
+  );
+});
